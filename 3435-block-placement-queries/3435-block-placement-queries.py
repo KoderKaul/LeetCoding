4d8f76d2ff625edbdb088ff330680bd403baeccc
Karan <62297273+KoderKaul@users.noreply.github.com>
2025-01-05 13:55:36 +0530

    Time: 2058 ms (90.4%), Space: 76.3 MB (91.2%) - LeetHub

--- 3435-block-placement-queries/3435-block-placement-queries.py
+++ 3435-block-placement-queries/3435-block-placement-queries.py
@@ -1,30 +1,43 @@
-class Solution:    
+class Solution:
     def getResults(self, queries: List[List[int]]) -> List[bool]:
         bit, res = [0] * (min(50000, 3 * len(queries)) + 1), []
-        
-        def bit_range(p, down=True):
-            while p >= 0 and p < len(bit):
-                yield p
-                p = (p & (p + 1)) - 1 if down else p | (p + 1)
-        
-        def get_max(p: int) -> int:
-            return max(bit[i] for i in bit_range(p))
-        
-        def update(p, val):
-            for idx in bit_range(p, False):
-                bit[idx] = max(bit[idx], val)
-        
+
+        def query(p: int) -> int:
+            """Get the maximum value in the Fenwick Tree up to index p."""
+            ans = 0
+            while p > 0:
+                ans = max(ans, bit[p])
+                p -= (p & -p)
+            return ans
+
+        def update(p: int, val: int):
+            """Update the Fenwick Tree with value `val` at index p."""
+            while p < len(bit):
+                bit[p] = max(bit[p], val)
+                p += (p & -p)
+
         blocks = [0] + sorted([x for t, x, *sz in queries if t == 1])
-        for i, b in enumerate(blocks[1:]):
-            update(b, b - blocks[i])
-        
+        # keep track of the gaps between blocks
+        for i in range(1, len(blocks)):
+            # Subtracting current block from the previous block
+            update(blocks[i], blocks[i] - blocks[i - 1])
+
+        #sari obstacles dal do fir ulti queries accept karo
+        #jabh type 1 aaega us time apn, us block ko remove kardenge using delete
+        # and next block ko update kardenge with new prev i.e curr ka prev so val=nxt-prev for nxt index
+        #in type2, you query 2 things - your x ka gap and the gap between x and prev => before prev or between x and prev you can put
         for t, x, *sz in reversed(queries):
-            p = bisect_left(blocks, x)
             if t == 1:
-                if p + 1 < len(blocks):
-                    update(blocks[p + 1], blocks[p + 1] - blocks[p - 1])
-                del blocks[p]
+                idx = bisect_left(blocks, x) # this will definately exist since we only added them 
+                prev, nxt = blocks[idx - 1], blocks[idx + 1] if idx + 1 < len(blocks) else len(bit)
+                del blocks[idx]
+                update(nxt, nxt - prev)
             else:
-                res.append(x - blocks[p - 1] >= sz[0] or get_max(x) >= sz[0])
-        
-        return (res[::-1])
+                idx = bisect_left(blocks, x)
+                prev_block = blocks[idx - 1]
+                # prev ka gap or x-lastBlock mein se max
+                max_gap = max(query(x), x - prev_block)
+
+                res.append(max_gap >= sz[0])
+
+        return res[::-1]
